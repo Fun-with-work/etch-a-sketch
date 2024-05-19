df09e5b861cf21e7816f51e7361a53573527fc57
aaman <aneela.aman.is@gmail.com>
2024-05-19 07:38:23 -0400

    add function to create divs

--- script.js
+++ script.js
@@ -7,18 +7,34 @@ const multiColor = document.getElementById("multi-color");
 const blackColor = document.getElementById("black-color");
 const eraseBoard = document.getElementById("erase-board");
 
-let gridSquares = document.getElementById("container");
+let myGrid = document.getElementById("container");
+
+// gridSquares.style.border = "5px ";
 
 
-gameStart.addEventListener("click", (e) => {
-    gridSquares.innerText = "It worked";
-});
+function create (n) {
+    let widHt = 600.00;
+    for (i=0; i<n**2; i++) {
+        let gridSquares = document.createElement("div");
+        gridSquares.style.color = "";
+        // gridSquares.style.boxSizing = "borderBox";
+        gridSquares.style.outline = "2px solid gray";
+        gridSquares.style.width = parseInt(widHt/n)+"px";
+        gridSquares.style.height = parseInt(widHt/n)+"px";
+        myGrid.appendChild(gridSquares);
+    }
+
+}
+
+create(16.00);
+
+// gameStart.addEventListener("click", (e) => {
+//     myGrid.innerText = "It worked";
+// });
 
 
 // const heading = document.getElementById("game");
 
 
-
-
-// function for creating divs
 // buttons for start, multicolor, black, erase, input from user
+
