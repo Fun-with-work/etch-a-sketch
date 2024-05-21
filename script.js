36ad602b4d511c043f8152cbeb3bf6f500904642
aaman <aneela.aman.is@gmail.com>
2024-05-21 10:15:37 -0400

    add function for user input and change divs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Etch-a-sketch
 
-const userInput = document.createElement("p");
+// const userInput = document.createElement("p");
 
 const gameStart = document.getElementById("game-start");
 const multiColor = document.getElementById("multi-color");
@@ -8,33 +8,54 @@ const blackColor = document.getElementById("black-color");
 const eraseBoard = document.getElementById("erase-board");
 
 let myGrid = document.getElementById("container");
+let num = 16.00;
+const widHt = 500.00;
 
-// gridSquares.style.border = "5px ";
-
-
-function create (n) {
-    let widHt = 600.00;
-    for (i=0; i<n**2; i++) {
-        let gridSquares = document.createElement("div");
+function create (num) {
+    for (i=0; i<num**2; i++) {
+        gridSquares = document.createElement("div");
+        gridSquares.className = "new-div";
         gridSquares.style.color = "";
-        // gridSquares.style.boxSizing = "borderBox";
+        gridSquares.style.backgroundColor = "#E6A8F7";
         gridSquares.style.outline = "2px solid gray";
-        gridSquares.style.width = parseInt(widHt/n)+"px";
-        gridSquares.style.height = parseInt(widHt/n)+"px";
+        gridSquares.style.width = Number(widHt/num)+"px";
+        gridSquares.style.height = Number(widHt/num)+"px";
         myGrid.appendChild(gridSquares);
     }
+};
 
-}
+create(num);
 
-create(16.00);
+function myNumber() {
+    let num = prompt ("How many squares per side do you want in the grid? Choose between 10 - 100", "16.00");
+    
+    if (num == null || num == "") {
+        alert ( "User cancelled the prompt.");
+    } else if (num < 10 || num >100) {
+        alert ( "The number is invalid.");
+    } else { 
+        while (myGrid.firstChild) myGrid.firstChild.remove();
+        create(num);
+    }};
+
+
+
+// function mouseOver() {
+
+
+    // for (i2=0; i2 <num**2; i2++)
+    // gridSquares.style.backgroundColor = "red";
+//   }
+
+// mouseOver();
 
 // gameStart.addEventListener("click", (e) => {
-//     myGrid.innerText = "It worked";
+//    for (i2=0; i2 <num**2; i2++)
+//     gridSquares.style.backgroundColor = "red";
 // });
 
 
 // const heading = document.getElementById("game");
 
 
-// buttons for start, multicolor, black, erase, input from user
 
